a069be4bca5fa50b33d49aae031bd4bf38a4df9b
Shishir Tripathi <97309627+vjrdnti@users.noreply.github.com>
2025-04-21 21:14:28 +0530

    Update settings.py
    allowing render to host

--- tbuffer/settings.py
+++ tbuffer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0cjhfn%y8l=-tw_1b&o(4smg&tqyr&n2(^+fr7qfg*^7%9c124
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [tbuffer.onrender.com]
+ALLOWED_HOSTS = ['tbuffer.onrender.com']
 
 
 # Application definition
