9f4ee7e2ec7bd33c58ecd1836e5421bf5dd0373e
Shishir Tripathi <97309627+vjrdnti@users.noreply.github.com>
2025-04-21 21:38:45 +0530

    Update apps.py
    ensuring /media/ exsists each time tbuffer_app runs

--- tbuffer_app/apps.py
+++ tbuffer_app/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class BufferAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'buffer_app'
+    name = 'tbuffer_app'
 
     def ready(self):
         os.makedirs("media", exist_ok=True)
